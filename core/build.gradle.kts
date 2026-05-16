a84e162e618393575079c28a95b998226f871b9f
David G Huss <34256796+thehomelessguy@users.noreply.github.com>
2026-05-15 23:18:04 -0400

    fix(core): add compileOnly dependencies for WPILib and CTRE bridging

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -7,10 +7,17 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://frcmaven.wpi.edu/artifactory/release/")
+    maven("https://maven.ctr-electronics.com/release/")
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    compileOnly("edu.wpi.first.wpilibj:wpilibj-java:2024.3.2")
+    compileOnly("edu.wpi.first.hal:hal-java:2024.3.2")
+    compileOnly("edu.wpi.first.wpimath:wpimath-java:2024.3.2")
+    compileOnly("edu.wpi.first.wpiutil:wpiutil-java:2024.3.2")
+    compileOnly("com.ctre.phoenix6:wpiapi-java:24.1.0")
 }
 
 tasks.test {
