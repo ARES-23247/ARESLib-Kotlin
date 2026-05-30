ae3ffb0d1d16d2fa368481cdf5b33c1e67ed05fb
David G Huss <34256796+thehomelessguy@users.noreply.github.com>
2026-05-30 14:39:01 -0400

    fix(ftc-mocks): annotate GoBildaPinpointDriver fields with @JvmField to prevent runtime NoSuchMethodError

--- ftc-mocks/src/main/kotlin/com/qualcomm/hardware/gobilda/GoBildaPinpointDriver.kt
+++ ftc-mocks/src/main/kotlin/com/qualcomm/hardware/gobilda/GoBildaPinpointDriver.kt
@@ -1,9 +1,12 @@
 package com.qualcomm.hardware.gobilda
 
 open class GoBildaPinpointDriver {
-    var posX: Double = 0.0
-    var posY: Double = 0.0
-    var heading: Double = 0.0
+    @JvmField var posX: Double = 0.0
+    @JvmField var posY: Double = 0.0
+    @JvmField var heading: Double = 0.0
+    @JvmField var velX: Double = 0.0
+    @JvmField var velY: Double = 0.0
+    @JvmField var headingVelocity: Double = 0.0
     
     fun update() {}
     fun resetPosAndIMU() {}
