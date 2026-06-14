11d789a53e80fd71760640e622cc211d7ceda931
David G Huss <34256796+thehomelessguy@users.noreply.github.com>
2026-06-14 18:31:18 -0400

    fix(mocks): fix Telemetry.addData return type in mock to prevent NoSuchMethodError on real Control Hub

--- ftc-mocks/src/main/kotlin/org/firstinspires/ftc/robotcore/external/Telemetry.kt
+++ ftc-mocks/src/main/kotlin/org/firstinspires/ftc/robotcore/external/Telemetry.kt
@@ -1,7 +1,8 @@
 package org.firstinspires.ftc.robotcore.external
 
 open class Telemetry {
-    open fun addData(caption: String, value: Any?): Any? = null
-    open fun addData(caption: String, format: String, vararg args: Any?): Any? = null
+    interface Item
+    open fun addData(caption: String, value: Any?): Item? = null
+    open fun addData(caption: String, format: String, vararg args: Any?): Item? = null
     open fun update(): Boolean = true
 }
