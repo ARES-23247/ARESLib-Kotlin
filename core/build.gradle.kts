a0904add9f6330b616e4e9626a9be9b8871ca734
Chickinmuggins <jules.huss@gmail.com>
2026-06-14 20:00:05 -0400

    chore: configure gradle build to use JDK 21 toolchain

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -26,7 +26,7 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 publishing {
--- simulator/build.gradle.kts
+++ simulator/build.gradle.kts
@@ -61,5 +61,5 @@ dependencies {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
